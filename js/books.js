a85f30eb866584bdde1101dd6c42c1ad88297955
Bartosz <b.redkowiak@gmail.com>
2019-10-18 16:10:05 +0200

    Zadanie 4

--- js/books.js
+++ js/books.js
@@ -1,24 +1,26 @@
-$(function () {
+$(function (events, handler) {
+
+//------------- Zadanie 3 -----------------------------------------------------------
 
     var tBody = $('#tBody');
 
-    $.ajax ({
+    $.ajax({
         url: "http://localhost:8282/books/",
         type: "GET",
         dataType: "json",
         contentType: "application/json"
-    }).done(function(result) {
-        for (var i = 0 ; i < result.length ; i++){
+    }).done(function (result) {
+        for (var i = 0; i < result.length; i++) {
             //Odbieram pierwszą książkę
             var book = result[i];
             //Tworzę nowy wiersz
             var tr = $('<tr>').addClass('bookRecord');
             //Tworzę kolumny
-            $('<td>').text(book.id).attr('id', 'bookId_' + book.id ).appendTo(tr);
-            $('<td>').text(book.title).attr('id', 'title_' + book.id )
+            $('<td>').text(book.id).attr('id', 'bookId_' + book.id).appendTo(tr);
+            $('<td>').text(book.title).attr('id', 'title_' + book.id).addClass('bookTitle')
                 .attr('data-method', 'GET').appendTo(tr);
-            $('<td>').text(book.author).attr('id', 'bookAuthor_' + book.id ).appendTo(tr);
-            $('<td><button class="removeBtn" data-method="DELETE">Delete</button></td>').appendTo(tr);
+            $('<td>').text(book.author).attr('id', 'bookAuthor_' + book.id).appendTo(tr);
+            $('<td><button class="btn btn-danger" data-method="DELETE">Delete</button></td>').appendTo(tr);
 
             //Podpinam wiersz pod tabelę na stronie
             tr.appendTo(tBody);
@@ -28,4 +30,23 @@ $(function () {
             hiddenTr.appendTo(tBody);
         }
     });
+
+//------------- Zadanie 4 -----------------------------------------------------------
+
+    tBody.on('click','.bookTitle' , function () {
+        var id = $(this).prev().text();
+        var bookDescription = $(this).parent().next().toggle(300);
+
+
+        $.ajax({
+            url: "http://localhost:8282/books/" + id,
+            type: "GET",
+            dataType: "json",
+            contentType: "application/json"
+        }).done(function (result) {
+            var input = 'ISBN: ' + result.isbn + '<br>Publisher: ' + result.publisher + '<br>Type: ' + result.type;
+            bookDescription.find('td').html(input);
+        });
+    });
+
 });
